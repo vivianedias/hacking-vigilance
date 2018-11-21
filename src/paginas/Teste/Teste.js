b922011fc185d482e26ce76a967d4c3a454b69d4
vivianedias <vivipirolad@gmail.com>
2018-11-21 01:49:55 -0200

    add navbar e menu na pag teste

--- src/paginas/Teste/Teste.js
+++ src/paginas/Teste/Teste.js
@@ -1,60 +1,63 @@
 import React, { Component } from 'react'
 import Checkbox from '@material-ui/core/Checkbox';
-import './Teste.css'
 import Sidebar from '../../componentes/Sidebar/Sidebar'
+import Navbar from '../../componentes/Navbar/Navbar'
+import './Teste.css'
 
 class Teste extends Component {
   
   render(){
 
     return(
-      <main className="container">
-
-        <Sidebar className="column bottom"/>
-      
-        <form className="column bottom">
-          <section>
-            <h1 id="item1">Item 1</h1>
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-          </section>
-          <section>
-            <h1 id="item2">Item 2</h1>
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-          </section>
-          <section>
-            <h1 id="item3">Item 3</h1>
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-          </section>
-          <section>
-            <h1 id="item4">Item 4</h1>
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-          </section>
-          <section>
-            <h1 id="item5">Item 5</h1>
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-              <Checkbox value="checkedC" />
-          </section>
-          <input type="submit" value="Submit" />
-        </form>
+      <main>
+        <Navbar />
+        <div className="container">
+          <Sidebar className="teste__column"/>
+          
+          <form className="teste__column">
+            <section>
+              <h1 id="item1">Item 1</h1>
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+            </section>
+            <section>
+              <h1 id="item2">Item 2</h1>
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+            </section>
+            <section>
+              <h1 id="item3">Item 3</h1>
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+            </section>
+            <section>
+              <h1 id="item4">Item 4</h1>
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+            </section>
+            <section>
+              <h1 id="item5">Item 5</h1>
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+                <Checkbox value="checkedC" />
+            </section>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </main>
     )
   }
