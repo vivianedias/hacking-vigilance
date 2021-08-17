f8033a55e7ad13767650dbe43da128e651ea76cb
Viviane Dias <vivipirolad@gmail.com>
2021-08-17 13:14:32 -0300

    Use auth0 to authenticate users

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { appWithTranslation } from "next-i18next";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </ChakraProvider>
   );
 }
