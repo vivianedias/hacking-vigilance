80bc9e15d41ab4816d3daca347aec5815fe8f9a2
Viviane Dias <vivipirolad@gmail.com>
2021-11-15 23:19:57 -0300

    feat(layout): adds bg color to PageLayout

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -1,10 +1,13 @@
 import { Navbar, Footer } from "../";
+import { Box } from "@chakra-ui/react";
 
 const PageLayout = ({ children }) => {
   return (
     <main>
       <Navbar />
-      {children}
+      <Box as="section" bgColor="gray.100" p="20px">
+        {children}
+      </Box>
       <Footer />
     </main>
   );
