4cad1d8171cd45f6789b10c479001e063784dc90
Viviane Dias <vivianedias@protonmail.com>
2021-11-20 10:28:59 -0300

    wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,9 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { PageLayout } from "../components";
 
-export default function Home() {
+export default function Home({ envs }) {
   const { t } = useTranslation("index");
-
+  console.log({ envs: JSON.parse(envs) });
   return (
     <div className="container">
       <Head>
@@ -19,9 +19,11 @@ export default function Home() {
 }
 
 export async function getStaticProps({ locale }) {
+  const { AIRTABLE_API_KEY, ...rest } = process.env;
   return {
     props: {
       ...(await serverSideTranslations(locale, ["index", "navbar", "footer"])),
+      envs: JSON.stringify(rest),
     },
   };
 }
