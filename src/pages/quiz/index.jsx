1bd7fd750e932624a1ddbf5fae61c924a82a1007
Viviane Dias <vivianedias@protonmail.com>
2021-11-20 10:34:20 -0300

    feat(qui): adds quiz context

---
+++ src/pages/quiz/index.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { PageLayout } from "../../components";
+
+export default function Quiz() {
+  const { t } = useTranslation("quiz");
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Quiz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <PageLayout>Quiz</PageLayout>
+    </div>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["quiz", "navbar", "footer"])),
+    },
+  };
+}
